b6f53341a5c62c1fb9da593986b919f26b95e4ab
Jr-Lopes <adm.cleberjr@gmail.com>
2025-06-15 16:13:02 -0300

    Atualizações

---
+++ Exemplos/008 Exemplo/ExemploC.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define N 50
+
+int main(){
+    char frase[N];
+    int i = 0;
+
+    printf("Digite uma frase: ");
+    gets(frase);
+
+    printf("Voce digitou: \n");
+    while(frase[i] != "\0")
+        if(frase[i++] != ' ')
+            putchar(frase[i - 1]);
+        else
+            putchar('\n');
+    printf("\n");
+
+    return 0;
+}
